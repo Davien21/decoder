d7cf981a82fd936e4f671fafeb5073f9f730df15
davien21 <chidiebereekennia@gmail.com>
2020-04-01 21:30:59 +0100

    Add Z conversion

--- decoder.rb
+++ decoder.rb
@@ -1,10 +1,11 @@
 KEY = {
-  1 => "A"
-  2 => "B"
-  3 => "C"
-  4 => "D"
-  5 => "E"
-  6 => "F"
+  1 => "A",
+  2 => "B",
+  3 => "C",
+  4 => "D",
+  5 => "E",
+  6 => "F",
+  26 => "Z",
 }
 
 # Define a method that takes an array of numbers to decode.
